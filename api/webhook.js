c508047b7f0e6283a375d03886528d41eaabffc0
mmoraiss <marciopokerpoa@gmail.com>
2026-03-06 22:37:17 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -12,13 +12,31 @@ export default async function handler(req, res) {
 
     const paymentId = body.data.id
 
-    console.log("Pagamento recebido:", paymentId)
+    console.log("Pagamento ID:", paymentId)
 
-    // aqui depois vamos consultar o pagamento
-    // e ativar a licença
+    const response = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN}`
+      }
+    })
+
+    const payment = await response.json()
+
+    console.log("Status pagamento:", payment.status)
+
+    if (payment.status === "approved") {
+
+      console.log("Pagamento aprovado!")
+
+      const clubId = payment.metadata?.clubId
+
+      console.log("Clube:", clubId)
+
+      // aqui vamos ativar a licença depois
+
+    }
 
   }
 
   res.status(200).send("ok")
-
 }
